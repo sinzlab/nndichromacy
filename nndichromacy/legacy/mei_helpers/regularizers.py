0f0f6dadae20131d64ed2963995fed87e84a6399
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-06-25 18:00:23 +0200

    rename mei module to mei_helpers and add contrast functions.

--- nndichromacy/legacy/mei_helpers/regularizers.py
+++ nndichromacy/legacy/mei_helpers/regularizers.py
@@ -1,6 +1,6 @@
-from featurevis.legacy.ops import ChangeStd, GaussianBlur, Jitter, TotalVariation, ChangeNorm, ClipRange
-from featurevis.legacy.utils import Compose
-from featurevis import utils, ops
+from nndichromacy.legacy.featurevis.ops import ChangeStd, GaussianBlur, Jitter, TotalVariation, ChangeNorm, ClipRange
+from nndichromacy.legacy.featurevis.utils import Compose
+from nndichromacy.legacy.featurevis import utils, ops
 from .utility import cumstom_initial_guess
 from functools import partial
 
@@ -11,6 +11,19 @@ postup_contrast_1 = ChangeStd(1)
 postup_contrast_125 = ChangeStd(12.5)
 postup_contrast_100 = ChangeStd(10)
 postup_contrast_5 = ChangeStd(5)
+
+postup_contrast_0075 = ChangeStd(0.075)
+postup_contrast_0125 = ChangeStd(0.125)
+postup_contrast_015 = ChangeStd(0.15)
+postup_contrast_0175 = ChangeStd(0.175)
+postup_contrast_02 = ChangeStd(0.2)
+postup_contrast_0225 = ChangeStd(0.225)
+postup_contrast_025 = ChangeStd(0.25)
+postup_contrast_0275 = ChangeStd(0.275)
+postup_contrast_03 = ChangeStd(0.3)
+postup_contrast_0325 = ChangeStd(0.325)
+postup_contrast_035 = ChangeStd(0.35)
+
 
 # Blurring
 Blur_sigma_1 = GaussianBlur(1)
