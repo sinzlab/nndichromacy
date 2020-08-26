a469f98a380ab0bd96bfc367a46b6eee93bad376
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-08-26 11:40:47 +0200

    remove monkey specific dataset functions

--- nndichromacy/datasets/__init__.py
+++ nndichromacy/datasets/__init__.py
@@ -1,2 +1,1 @@
-from .monkey_loaders import monkey_static_loader, monkey_mua_sua_loader
 from .mouse_loaders import static_loaders, static_shared_loaders
--- nndichromacy/datasets/conventions.py
+++
@@ -1,7 +0,0 @@
-
-
-unit_type_conventions = {
-    "isolated_neuron_depth_probe": 1,
-    "MUA": 10,
-    "LFP": 20,
-}
