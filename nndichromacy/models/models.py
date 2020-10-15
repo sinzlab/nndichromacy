6c8d38fba2775f7716e6d5d5aabd870a7b76468d
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-10-15 14:33:25 +0200

    bug fix se_core_point_readout with new Encoder class.

--- nndichromacy/models/models.py
+++ nndichromacy/models/models.py
@@ -347,23 +347,6 @@ def se_core_point_readout(dataloaders, seed, hidden_channels=32, input_kern=13, 
 
     core_input_channels = list(input_channels.values())[0] if isinstance(input_channels, dict) else input_channels[0]
 
-    class Encoder(nn.Module):
-
-        def __init__(self, core, readout, elu_offset):
-            super().__init__()
-            self.core = core
-            self.readout = readout
-            self.offset = elu_offset
-
-        def forward(self, x, data_key=None, **kwargs):
-            x = self.core(x)
-
-            x = self.readout(x, data_key=data_key)
-            return F.elu(x + self.offset) + 1
-
-        def regularizer(self, data_key):
-            return self.core.regularizer() + self.readout.regularizer(data_key=data_key)
-
     set_random_seed(seed)
 
     core = SE2dCore(input_channels=core_input_channels,
