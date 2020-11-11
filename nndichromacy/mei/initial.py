a0bcbd7e1db7aabe0cedaba317b4752cb90b2aa4
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-11-12 00:24:59 +0100

    add CustomRandomNormal to mei intial module towork with latest mei package.

--- nndichromacy/mei/initial.py
+++ nndichromacy/mei/initial.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 from mei.initial import InitialGuessCreator
+import torch
 from torch import Tensor, randn
-from .utility import cumstom_initial_guess
-from functools import partial
+
+
+def cumstom_initial_guess(*args, mean=0, std=1, device="cuda"):
+    return torch.empty(*[args]).normal_(mean=mean, std=std).to(device)
+
 
 class RandomNormalBehavior(InitialGuessCreator):
     """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
@@ -27,4 +31,18 @@ class RandomNormalBehavior(InitialGuessCreator):
         return f"{self.__class__.__qualname__}()"
 
 
-eightbit_initial_guess = partial(cumstom_initial_guess, mean=111, std=60)
+class CustomRandomNormal(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    _create_random_tensor = staticmethod(cumstom_initial_guess)
+
+    def __init__(self, mean=111, std=60):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, *shape, mean=111, std=60):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape."""
+        return self._create_random_tensor(*shape, mean=self.mean, std=self.std)
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
