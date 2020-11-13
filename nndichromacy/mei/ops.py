99d58b6aaa2f77fdb98334ea953da221de41884e
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-11-13 15:41:30 +0100

    Add std-constraint with clamped mean method to mei module.

--- nndichromacy/mei/ops.py
+++ nndichromacy/mei/ops.py
@@ -200,3 +200,32 @@ class ChangeNormShuffleBehavior:
         x[:, self.first_behav_channel+1, ...] = np.random.uniform(self.dpupil_limits[0], self.dpupil_limits[1])
         x[:, self.first_behav_channel+2, ...] = np.random.uniform(self.treadmill_limits[0], self.treadmill_limits[1])
         return x
+
+
+class ChangeStdClampedMean:
+    """ Change the norm of the input.
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, std, x_min, x_max, clamped_mean):
+        self.clamped_mean = clamped_mean
+        self.std = std
+        self.x_min = x_min
+        self.x_max = x_max
+        self.clamped_mean = clamped_mean
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x = x.clamp(self.x_min,  self.x_max)
+        x_std = torch.std(x.view(len(x), -1), dim=-1)
+
+        # set x to have the desired std
+        x = x * (self.std / (x_std + 1e-9)).view(len(x), *[1] * (x.dim() - 1))
+        # compute mean of x
+        x_mean = torch.mean(x.view(len(x), -1), dim=-1)
+        # set mean to the clamped value
+        x = x + (self.clamped_mean - x_mean).view(len(x), *[1] * (x.dim() - 1))
+        return x
