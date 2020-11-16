f09f2edff5de555f16e79cd84ce91fb5bfa09e89
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-11-16 12:02:27 +0100

    Add RandomNormalSelectedChannels clss to mei.initial module.

--- nndichromacy/mei/initial.py
+++ nndichromacy/mei/initial.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from mei.initial import InitialGuessCreator
 import torch
 from torch import Tensor, randn
+from collections import Iterable
 
 
 def cumstom_initial_guess(*args, mean=0, std=1, device="cuda"):
@@ -25,6 +26,30 @@ class RandomNormalBehavior(InitialGuessCreator):
         inital[:, self.first_behav_channel, ...] = self.channel_0
         inital[:, self.first_behav_channel+1, ...] = self.channel_1
         inital[:, self.first_behav_channel+2, ...] = self.channel_2
+        return inital
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
+
+
+class RandomNormalSelectedChannels(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    _create_random_tensor = randn
+
+    def __init__(self, selected_channels, selected_values):
+        if not isinstance(selected_channels, Iterable):
+            self.selected_channels = (selected_channels)
+
+        if not isinstance(selected_values, Iterable):
+            self.selected_values = (selected_values)
+
+    def __call__(self, *shape):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape."""
+        inital = self._create_random_tensor(*shape)
+        for channel, value in zip(self.selected_channels, self.selected_values):
+            inital[:, channel, ...] = value
+
         return inital
 
     def __repr__(self):
