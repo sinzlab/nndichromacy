8f21087ae432d0235a6a2cb7bdaab671a55fcb10
Fabian Sinz <fabian.sinz@uni-tuebingen.de>
2020-12-02 21:19:54 +0100

    attention readout

--- nndichromacy/models/models.py
+++ nndichromacy/models/models.py
@@ -10,7 +10,8 @@ from torch.nn import functional as F
 
 from .cores import SE2dCore, TransferLearningCore
 from .encoders import Encoder
-from .readouts import MultipleFullGaussian2d, MultiReadout, MultipleSpatialXFeatureLinear, MultipleRemappedGaussian2d
+from .readouts import MultipleFullGaussian2d, MultiReadout, MultipleSpatialXFeatureLinear, MultipleRemappedGaussian2d, \
+    MultipleAttention2d
 from .shifters import MLPShifter, StaticAffine2dShifter
 from .utility import *
 
@@ -448,6 +449,75 @@ def se_core_remapped_gauss_readout(dataloaders, seed, hidden_channels=32, input_
                     readout=readout,
                     elu_offset=elu_offset,
                     shifter=shifter)
+
+    return model
+
+
+def se_core_attention_readout(dataloaders, seed, hidden_channels=32, input_kern=13,  # core args
+                                   hidden_kern=3, layers=3, gamma_input=15.5,
+                                   skip=0, final_nonlinearity=True, momentum=0.9,
+                                   pad_input=False, batch_norm=True, hidden_dilation=1,
+                                   laplace_padding=None, input_regularizer='LaplaceL2norm',
+                                   readout_bias=True,  # readout args,
+                                   gamma_readout=4, elu_offset=0, stack=None, se_reduction=32, n_se_blocks=1,
+                                   depth_separable=False, linear=False,
+                                   attention_layers=2, attention_kernel=3, data_info=None):
+
+    if data_info is not None:
+        n_neurons_dict, in_shapes_dict, input_channels = unpack_data_info(data_info)
+    else:
+        if "train" in dataloaders.keys():
+            dataloaders = dataloaders["train"]
+
+        # Obtain the named tuple fields from the first entry of the first dataloader in the dictionary
+        in_name, out_name = next(iter(list(dataloaders.values())[0]))._fields[:2]
+
+        session_shape_dict = get_dims_for_loader_dict(dataloaders)
+        n_neurons_dict = {k: v[out_name][1] for k, v in session_shape_dict.items()}
+        in_shapes_dict = {k: v[in_name] for k, v in session_shape_dict.items()}
+        input_channels = [v[in_name][1] for v in session_shape_dict.values()]
+
+    core_input_channels = list(input_channels.values())[0] if isinstance(input_channels, dict) else input_channels[0]
+
+    set_random_seed(seed)
+
+    core = SE2dCore(input_channels=core_input_channels,
+                    hidden_channels=hidden_channels,
+                    input_kern=input_kern,
+                    hidden_kern=hidden_kern,
+                    layers=layers,
+                    gamma_input=gamma_input,
+                    skip=skip,
+                    final_nonlinearity=final_nonlinearity,
+                    bias=False,
+                    momentum=momentum,
+                    pad_input=pad_input,
+                    batch_norm=batch_norm,
+                    hidden_dilation=hidden_dilation,
+                    laplace_padding=laplace_padding,
+                    input_regularizer=input_regularizer,
+                    stack=stack,
+                    se_reduction=se_reduction,
+                    n_se_blocks=n_se_blocks,
+                    depth_separable=depth_separable,
+                    linear=linear,
+                    attention_conv=False)
+
+    readout = MultipleAttention2d(core, in_shape_dict=in_shapes_dict,
+                                         n_neurons_dict=n_neurons_dict,
+                                         bias=readout_bias,
+                                         gamma_readout=gamma_readout,
+                                         attention_layers=attention_layers, attention_kernel=attention_kernel)
+
+    # initializing readout bias to mean response
+    if readout_bias and data_info is None:
+        for key, value in dataloaders.items():
+            _, targets = next(iter(value))[:2]
+            readout[key].bias.data = targets.mean(0)
+
+    model = Encoder(core=core,
+                    readout=readout,
+                    elu_offset=elu_offset)
 
     return model
 
--- nndichromacy/models/readouts.py
+++ nndichromacy/models/readouts.py
@@ -1,24 +1,43 @@
 import torch
 from neuralpredictors.layers.legacy import Gaussian2d
-from neuralpredictors.layers.readouts import PointPooled2d, FullGaussian2d, SpatialXFeatureLinear, RemappedGaussian2d
+from neuralpredictors.layers.readouts import (
+    PointPooled2d,
+    FullGaussian2d,
+    SpatialXFeatureLinear,
+    RemappedGaussian2d,
+    AttentionReadout,
+)
 from nnfabrik.utility.nn_helpers import get_module_output
 
 
 class MultiplePointPooled2d(torch.nn.ModuleDict):
-    def __init__(self, core, in_shape_dict, n_neurons_dict, pool_steps, pool_kern, bias, init_range, gamma_readout):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        pool_steps,
+        pool_kern,
+        bias,
+        init_range,
+        gamma_readout,
+    ):
         # super init to get the _module attribute
         super(MultiplePointPooled2d, self).__init__()
         for k in n_neurons_dict:
             in_shape = get_module_output(core, in_shape_dict[k])[1:]
             n_neurons = n_neurons_dict[k]
-            self.add_module(k, PointPooled2d(
-                in_shape,
-                n_neurons,
-                pool_steps=pool_steps,
-                pool_kern=pool_kern,
-                bias=bias,
-                init_range=init_range)
-                            )
+            self.add_module(
+                k,
+                PointPooled2d(
+                    in_shape,
+                    n_neurons,
+                    pool_steps=pool_steps,
+                    pool_kern=pool_kern,
+                    bias=bias,
+                    init_range=init_range,
+                ),
+            )
         self.gamma_readout = gamma_readout
 
     def forward(self, *args, data_key=None, **kwargs):
@@ -31,19 +50,31 @@ class MultiplePointPooled2d(torch.nn.ModuleDict):
 
 
 class MultipleGaussian2d(torch.nn.ModuleDict):
-    def __init__(self, core, in_shape_dict, n_neurons_dict, init_mu_range, init_sigma_range, bias, gamma_readout):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        init_mu_range,
+        init_sigma_range,
+        bias,
+        gamma_readout,
+    ):
         # super init to get the _module attribute
         super(MultipleGaussian2d, self).__init__()
         for k in n_neurons_dict:
             in_shape = get_module_output(core, in_shape_dict[k])[1:]
             n_neurons = n_neurons_dict[k]
-            self.add_module(k, Gaussian2d(
-                in_shape=in_shape,
-                outdims=n_neurons,
-                init_mu_range=init_mu_range,
-                init_sigma_range=init_sigma_range,
-                bias=bias)
-                            )
+            self.add_module(
+                k,
+                Gaussian2d(
+                    in_shape=in_shape,
+                    outdims=n_neurons,
+                    init_mu_range=init_mu_range,
+                    init_sigma_range=init_sigma_range,
+                    bias=bias,
+                ),
+            )
         self.gamma_readout = gamma_readout
 
     def forward(self, *args, data_key=None, **kwargs):
@@ -56,7 +87,6 @@ class MultipleGaussian2d(torch.nn.ModuleDict):
 
 
 class MultiReadout:
-
     def forward(self, *args, data_key=None, **kwargs):
         if data_key is None and len(self) == 1:
             data_key = list(self.keys())[0]
@@ -67,20 +97,31 @@ class MultiReadout:
 
 
 class MultipleSpatialXFeatureLinear(MultiReadout, torch.nn.ModuleDict):
-    def __init__(self, core, in_shape_dict, n_neurons_dict, init_noise, bias, normalize, gamma_readout):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        init_noise,
+        bias,
+        normalize,
+        gamma_readout,
+    ):
         # super init to get the _module attribute
         super().__init__()
         for k in n_neurons_dict:
             in_shape = get_module_output(core, in_shape_dict[k])[1:]
             n_neurons = n_neurons_dict[k]
-            self.add_module(k, SpatialXFeatureLinear(
-                in_shape=in_shape,
-                outdims=n_neurons,
-                init_noise=init_noise,
-                bias=bias,
-                normalize=normalize
+            self.add_module(
+                k,
+                SpatialXFeatureLinear(
+                    in_shape=in_shape,
+                    outdims=n_neurons,
+                    init_noise=init_noise,
+                    bias=bias,
+                    normalize=normalize,
+                ),
             )
-                            )
         self.gamma_readout = gamma_readout
 
     def regularizer(self, data_key):
@@ -88,9 +129,23 @@ class MultipleSpatialXFeatureLinear(MultiReadout, torch.nn.ModuleDict):
 
 
 class MultipleFullGaussian2d(MultiReadout, torch.nn.ModuleDict):
-    def __init__(self, core, in_shape_dict, n_neurons_dict, init_mu_range, init_sigma, bias, gamma_readout,
-                 gauss_type, grid_mean_predictor, grid_mean_predictor_type, source_grids,
-                 share_features, share_grid, shared_match_ids, ):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        init_mu_range,
+        init_sigma,
+        bias,
+        gamma_readout,
+        gauss_type,
+        grid_mean_predictor,
+        grid_mean_predictor_type,
+        source_grids,
+        share_features,
+        share_grid,
+        shared_match_ids,
+    ):
         # super init to get the _module attribute
         super().__init__()
         k0 = None
@@ -103,46 +158,68 @@ class MultipleFullGaussian2d(MultiReadout, torch.nn.ModuleDict):
             shared_grid = None
             shared_transform = None
             if grid_mean_predictor is not None:
-                if grid_mean_predictor_type == 'cortex':
+                if grid_mean_predictor_type == "cortex":
                     source_grid = source_grids[k]
                 else:
-                    raise KeyError('grid mean predictor {} does not exist'.format(grid_mean_predictor_type))
+                    raise KeyError(
+                        "grid mean predictor {} does not exist".format(
+                            grid_mean_predictor_type
+                        )
+                    )
 
             elif share_grid:
                 shared_grid = {
-                    'match_ids': shared_match_ids[k],
-                    'shared_grid': None if i == 0 else self[k0].shared_grid
+                    "match_ids": shared_match_ids[k],
+                    "shared_grid": None if i == 0 else self[k0].shared_grid,
                 }
 
             if share_features:
                 shared_features = {
-                    'match_ids': shared_match_ids[k],
-                    'shared_features': None if i == 0 else self[k0].shared_features
+                    "match_ids": shared_match_ids[k],
+                    "shared_features": None if i == 0 else self[k0].shared_features,
                 }
             else:
                 shared_features = None
 
-            self.add_module(k, FullGaussian2d(
-                in_shape=in_shape,
-                outdims=n_neurons,
-                init_mu_range=init_mu_range,
-                init_sigma=init_sigma,
-                bias=bias,
-                gauss_type=gauss_type,
-                grid_mean_predictor=grid_mean_predictor,
-                shared_features=shared_features,
-                shared_grid=shared_grid,
-                source_grid=source_grid,
+            self.add_module(
+                k,
+                FullGaussian2d(
+                    in_shape=in_shape,
+                    outdims=n_neurons,
+                    init_mu_range=init_mu_range,
+                    init_sigma=init_sigma,
+                    bias=bias,
+                    gauss_type=gauss_type,
+                    grid_mean_predictor=grid_mean_predictor,
+                    shared_features=shared_features,
+                    shared_grid=shared_grid,
+                    source_grid=source_grid,
+                ),
             )
-                            )
         self.gamma_readout = gamma_readout
 
 
 class MultipleRemappedGaussian2d(MultiReadout, torch.nn.ModuleDict):
-    def __init__(self, core, in_shape_dict, n_neurons_dict, remap_layers, remap_kernel, max_remap_amplitude,
-                 init_mu_range, init_sigma, bias, gamma_readout,
-                 gauss_type, grid_mean_predictor, grid_mean_predictor_type, source_grids,
-                 share_features, share_grid, shared_match_ids, ):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        remap_layers,
+        remap_kernel,
+        max_remap_amplitude,
+        init_mu_range,
+        init_sigma,
+        bias,
+        gamma_readout,
+        gauss_type,
+        grid_mean_predictor,
+        grid_mean_predictor_type,
+        source_grids,
+        share_features,
+        share_grid,
+        shared_match_ids,
+    ):
         # super init to get the _module attribute
         super().__init__()
         k0 = None
@@ -155,39 +232,77 @@ class MultipleRemappedGaussian2d(MultiReadout, torch.nn.ModuleDict):
             shared_grid = None
             shared_transform = None
             if grid_mean_predictor is not None:
-                if grid_mean_predictor_type == 'cortex':
+                if grid_mean_predictor_type == "cortex":
                     source_grid = source_grids[k]
                 else:
-                    raise KeyError('grid mean predictor {} does not exist'.format(grid_mean_predictor_type))
+                    raise KeyError(
+                        "grid mean predictor {} does not exist".format(
+                            grid_mean_predictor_type
+                        )
+                    )
 
             elif share_grid:
                 shared_grid = {
-                    'match_ids': shared_match_ids[k],
-                    'shared_grid': None if i == 0 else self[k0].shared_grid
+                    "match_ids": shared_match_ids[k],
+                    "shared_grid": None if i == 0 else self[k0].shared_grid,
                 }
 
             if share_features:
                 shared_features = {
-                    'match_ids': shared_match_ids[k],
-                    'shared_features': None if i == 0 else self[k0].shared_features
+                    "match_ids": shared_match_ids[k],
+                    "shared_features": None if i == 0 else self[k0].shared_features,
                 }
             else:
                 shared_features = None
 
-            self.add_module(k, RemappedGaussian2d(
-                in_shape=in_shape,
-                outdims=n_neurons,
-                remap_layers=remap_layers,
-                remap_kernel=remap_kernel,
-                max_remap_amplitude=max_remap_amplitude,
-                init_mu_range=init_mu_range,
-                init_sigma=init_sigma,
-                bias=bias,
-                gauss_type=gauss_type,
-                grid_mean_predictor=grid_mean_predictor,
-                shared_features=shared_features,
-                shared_grid=shared_grid,
-                source_grid=source_grid,
+            self.add_module(
+                k,
+                RemappedGaussian2d(
+                    in_shape=in_shape,
+                    outdims=n_neurons,
+                    remap_layers=remap_layers,
+                    remap_kernel=remap_kernel,
+                    max_remap_amplitude=max_remap_amplitude,
+                    init_mu_range=init_mu_range,
+                    init_sigma=init_sigma,
+                    bias=bias,
+                    gauss_type=gauss_type,
+                    grid_mean_predictor=grid_mean_predictor,
+                    shared_features=shared_features,
+                    shared_grid=shared_grid,
+                    source_grid=source_grid,
+                ),
             )
-                            )
+        self.gamma_readout = gamma_readout
+
+
+class MultipleAttention2d(MultiReadout, torch.nn.ModuleDict):
+    def __init__(
+        self,
+        core,
+        in_shape_dict,
+        n_neurons_dict,
+        attention_layers,
+        attention_kernel,
+        bias,
+        gamma_readout,
+    ):
+        # super init to get the _module attribute
+        super().__init__()
+        k0 = None
+        for i, k in enumerate(n_neurons_dict):
+            k0 = k0 or k
+            in_shape = get_module_output(core, in_shape_dict[k])[1:]
+            n_neurons = n_neurons_dict[k]
+
+            self.add_module(
+                k,
+                AttentionReadout(
+                    in_shape=in_shape,
+                    outdims=n_neurons,
+                    attention_layers=attention_layers,
+                    attention_kernel=attention_kernel,
+                    bias=bias
+                ),
+            )
         self.gamma_readout = gamma_readout
