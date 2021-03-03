901b3da4d8a258eedd8db604415eb53ba61fcb25
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-03-03 10:31:42 +0100

    MEI production run.py

--- run.py
+++ run.py
@@ -1,26 +1,23 @@
-#!/usr/bin/python3
-
-# DJ and utils imports
 import datajoint as dj
-import time
-import tarfile
-from shutil import copyfile
-import os
 
 dj.config["enable_python_native_blobs"] = True
-dj.config["nnfabrik.schema_name"]= 'nnfabrik_color_mei'
-schema = dj.schema('nnfabrik_color_mei')
+dj.config['nnfabrik.schema_name'] = "nnfabrik_color_mei"
+dj.config["display.limit"] = 70
+schema = dj.schema("nnfabrik_color_mei")
 
-# copy data from QB to $SCRATCH volume
-os.makedirs('/data/mouse/toliaslab/static/')
+from nndichromacy.tables.from_mei import MEI, MEIMethod, MEISeed, MEISelector, TrainedEnsembleModel
 
-
-# project specific imports
 from nndichromacy.tables.from_nnfabrik import TrainedModel
+from nndichromacy.tables.from_mei import MEI, MEIMethod, MEISeed, MEISelector, TrainedEnsembleModel
 
-dataset_hashes = ['f6b2477ef169c1e1f40b9d125cb2b520']
-for dataset_hash in dataset_hashes:
-    pop_key = {'dataset_hash': dataset_hash,
-               'trainer_hash': '0d06f037501e129d11aa288d8f22788f',
-               'model_hash': 'af90cf960a14d3ea4b8d6138d4510693'}
-    TrainedModel.populate(pop_key, display_progress=True, reserve_jobs=True)
+
+
+for dataset_hash in ['eaae92b3f2c085cf5b35d0faddc63c15']:
+    for method_hash in ['4f899fe8aaadbebf734c46f167028614', '782dfe2d7c6c0be6d3d018419646fc4c']:
+
+        mei_key = dict(method_hash=method_hash, dataset_hash=dataset_hash)
+        #unit_keys = (SignalToNoiseRatio.Units & mei_key & "unit_snr > 0.3").fetch("KEY", limit=300)
+        #pop_key = dj.AndList([unit_keys, mei_key])
+        #MEI.populate(pop_key, display_progress=True, order='random', reserve_jobs=True)
+
+        MEI.populate(mei_key, display_progress=True, order='random', reserve_jobs=True)
