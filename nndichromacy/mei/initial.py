bccd9484dd5f7bf0a1f84002d560295eb0faebd3
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-03-25 00:42:30 +0100

    Add NaturalImgBehavior Initial Image.

--- nndichromacy/mei/initial.py
+++ nndichromacy/mei/initial.py
@@ -3,6 +3,8 @@ from mei.initial import InitialGuessCreator
 import torch
 from torch import Tensor, randn
 from collections import Iterable
+from dataport.bcm.color_mei.schema import StaticImage
+from ..tables.utils import preprocess_img_for_reconstruction
 
 
 def cumstom_initial_guess(*args, mean=0, std=1, device="cuda"):
@@ -23,6 +25,56 @@ class RandomNormalBehavior(InitialGuessCreator):
     def __call__(self, *shape):
         """Creates a random initial guess from which to start the MEI optimization process given a shape."""
         inital = self._create_random_tensor(*shape)
+        inital[:, self.first_behav_channel, ...] = self.channel_0
+        inital[:, self.first_behav_channel+1, ...] = self.channel_1
+        inital[:, self.first_behav_channel+2, ...] = self.channel_2
+        return inital
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
+
+
+class NaturalImgBehavior(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    def __init__(self, image_class, image_id, first_behav_channel, channel_0, channel_1, channel_2, img_size=(36, 64)):
+
+        image = (StaticImage.Image & dict(image_class=image_class, image_id=image_id)).fetch1("image")
+        image = preprocess_img_for_reconstruction(image, img_size=img_size)
+        # standardization
+        image = (image-image.mean()) / image.std()
+        self.natural_img = image
+        self.first_behav_channel = first_behav_channel
+        self.channel_0 = channel_0
+        self.channel_1 = channel_1
+        self.channel_2 = channel_2
+
+    def __call__(self, *shape):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape."""
+        initial = torch.ones(*shape).cuda()
+        n_channels_img = self.natural_img.shape[1]
+        initial[:, :n_channels_img, ...] = self.natural_img
+        initial[:, self.first_behav_channel, ...] = self.channel_0
+        initial[:, self.first_behav_channel + 1, ...] = self.channel_1
+        initial[:, self.first_behav_channel + 2, ...] = self.channel_2
+        return initial
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
+
+
+class GrayNormalBehavior(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    def __init__(self, first_behav_channel, channel_0, channel_1, channel_2):
+        self.first_behav_channel = first_behav_channel
+        self.channel_0 = channel_0
+        self.channel_1 = channel_1
+        self.channel_2 = channel_2
+
+    def __call__(self, *shape):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape."""
+        inital = torch.zeros(*shape)
         inital[:, self.first_behav_channel, ...] = self.channel_0
         inital[:, self.first_behav_channel+1, ...] = self.channel_1
         inital[:, self.first_behav_channel+2, ...] = self.channel_2
