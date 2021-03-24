560a22468c37bad289c192a30375d17f66fc368a
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-03-25 00:41:46 +0100

    remove debug printout.

--- nndichromacy/mei/targets.py
+++ nndichromacy/mei/targets.py
@@ -3,5 +3,4 @@ import torch
 
 def gauss_loss(output, responses, mean=False):
     loss = - torch.sum((output - responses)**2) if mean is False else - torch.mean((output - responses)**2)
-    print(loss)
     return loss
