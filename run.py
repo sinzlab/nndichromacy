f7424e1b304e5d7b799c9d402d1dfb8b507045db
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-04-01 15:19:15 +0200

    Add debug printout for slurm.

--- run.py
+++ run.py
@@ -6,13 +6,14 @@ dj.config['nnfabrik.schema_name'] = "nnfabrik_color_mei"
 dj.config["display.limit"] = 70
 schema = dj.schema("nnfabrik_color_mei")
 
-from nndichromacy.tables.from_mei import MEI, MEIMethod, MEISeed, MEISelector, TrainedEnsembleModel
-from nndichromacy.tables.scores import CorrelationToAvergeEnsemble
+from nndichromacy.tables.from_mei import MEI, MEIMethod, MEISeed
+
+from nndichromacy.mei.initial import RandomNormalBehavior
+print("import successful")
 
 key = {'dataset_hash': 'a814c139b67e879a351442d71f62f3c2'}
-
-
-method_keys = (MEIMethod & "method_ts >'2021-03-30'").fetch("KEY")
+from nndichromacy.tables.scores import TestCorrelationEnsemble, CorrelationToAvergeEnsemble
+method_keys = (MEIMethod & "method_ts >'2021-03-31'").fetch("KEY")
 unit_keys = (CorrelationToAvergeEnsemble.Units & key).fetch("KEY", limit=150, order_by="unit_avg_correlation DESC")
 
 mei_key = dj.AndList([unit_keys, method_keys, dict(dataset_hash='a814c139b67e879a351442d71f62f3c2')])
