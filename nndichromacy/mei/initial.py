bc93b45ef1f02f0e62edd27efaf9d64e1d2f2a6c
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-06-08 21:21:17 +0200

    Add new mei Initial class for adaptive pupil selection.

--- nndichromacy/mei/initial.py
+++ nndichromacy/mei/initial.py
@@ -141,6 +141,56 @@ class RandomNormBehaviorPositions(InitialGuessCreator):
         return f"{self.__class__.__qualname__}()"
 
 
+class RandomNormBehaviorPositionsAdaptivePupil(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    _create_random_tensor = randn
+
+    def __init__(self, selected_channels, selected_values, key, threshold_percentile):
+        if not isinstance(selected_channels, Iterable) and (selected_channels is not None):
+            selected_channels = (selected_channels)
+
+        if not isinstance(selected_values, Iterable) and (selected_values is not None):
+            selected_values = (selected_values)
+
+        self.selected_channels = selected_channels
+        self.selected_values = selected_values
+
+        from ..tables.from_nnfabrik import Dataset
+        dataloaders = (Dataset & key).get_dataloader()
+        behaviors = []
+        for b in dataloaders["train"][key["data_key"]]:
+            behaviors.append(b.behavior.cpu().numpy())
+        behaviors = np.vstack(behaviors)
+
+        pupil_threshold = np.percentile(behaviors[:, 0], threshold_percentile)
+        pupil = behaviors[:, 0].max()
+        while pupil > pupil_threshold:
+            randint = np.random.choice(range(behaviors.shape[0]))
+            pupil = behaviors[randint, 0]
+            running = behaviors[randint, 2]
+
+        self.selected_values[0] = pupil
+        self.selected_values[2] = running
+        print("done. Selected Behaviors: ", pupil, running)
+
+
+
+    def __call__(self, *shape):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape."""
+
+        inital = self._create_random_tensor(*shape)
+        if self.selected_channels is not None:
+            for channel, value in zip(self.selected_channels, self.selected_values):
+                inital[:, channel, ...] = value.item() if not (isinstance(value, float) or isinstance(value, int)) else value
+        inital[:, -2:, ...] = torch.from_numpy(np.stack(np.meshgrid(np.linspace(-1, 1, shape[-1]), np.linspace(-1, 1, shape[-2]))))
+        return inital
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
+
+
+
 class CustomRandomNormal(InitialGuessCreator):
     """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
 
