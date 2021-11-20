d4a87875fde6629d1c8cba6fcd79a44d7a37a315
ShanqMa <shanqianma@gmail.com>
2021-11-20 14:50:52 +0100

    add surrMEI,ringMEI,contrastiveMEI,compatible for color MEI generation

--- nndichromacy/mei/ops.py
+++ nndichromacy/mei/ops.py
@@ -59,9 +59,69 @@ class BlurAndCut:
         final_x = blurred_x / (y_gaussian.sum() * x_gaussian.sum())  # normalize
         if self.cut_channel is not None:
             final_x[:, self.cut_channel, ...] *= 0
-
         return final_x
 
+class BlurAndCutSurr:
+    """ Blur an image with a Gaussian window.
+
+    Arguments:
+        sigma (float or tuple): Standard deviation in y, x used for the gaussian blurring.
+        decay_factor (float): Compute sigma every iteration as `sigma + decay_factor *
+            (iteration - 1)`. Ignored if None.
+        truncate (float): Gaussian window is truncated after this number of standard
+            deviations to each side. Size of kernel = 8 * sigma + 1
+        pad_mode (string): Mode for the padding used for the blurring. Valid values are:
+            'constant', 'reflect' and 'replicate'
+    """
+
+    def __init__(self, sigma, key, decay_factor=None, truncate=4, pad_mode="reflect", cut_channel=None,mask_thres=0.3):
+        self.sigma = sigma if isinstance(sigma, tuple) else (sigma,) * 2
+        self.decay_factor = decay_factor
+        self.truncate = truncate
+        self.pad_mode = pad_mode
+        self.cut_channel = cut_channel
+        if cut_channel is not None:
+            print(f"cutting channel: {cut_channel}")
+
+        src_method_fn = key["src_method_fn"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+
+        unit_id = key["unit_id"]
+
+        inner_mei_path = (MEI & dict(method_fn=src_method_fn) & dict(ensemble_hash=inner_ensemble_hash) & dict(method_hash=inner_method_hash) & dict(unit_id=unit_id)).fetch1('mei', download_path=fetch_download_path)
+        
+        inner_mei=torch.load(inner_mei_path)
+        self.center_mask= (inner_mei[0][-1] > mask_thres) * 1
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        num_channels = x.shape[1]
+
+        # Update sigma if needed
+        if self.decay_factor is None:
+            sigma = self.sigma
+        else:
+            sigma = tuple(s + self.decay_factor * (iteration - 1) for s in self.sigma)
+
+        # Define 1-d kernels to use for blurring
+        y_halfsize = max(int(round(sigma[0] * self.truncate)), 1)
+        y_gaussian = signal.gaussian(2 * y_halfsize + 1, std=sigma[0])
+        x_halfsize = max(int(round(sigma[1] * self.truncate)), 1)
+        x_gaussian = signal.gaussian(2 * x_halfsize + 1, std=sigma[1])
+        y_gaussian = torch.as_tensor(y_gaussian, device=x.device, dtype=x.dtype)
+        x_gaussian = torch.as_tensor(x_gaussian, device=x.device, dtype=x.dtype)
+
+        # Blur
+        padded_x = F.pad(x, pad=(x_halfsize, x_halfsize, y_halfsize, y_halfsize), mode=self.pad_mode)
+        blurred_x = F.conv2d(padded_x, y_gaussian.repeat(num_channels, 1, 1)[..., None], groups=num_channels)
+        blurred_x = F.conv2d(blurred_x, x_gaussian.repeat(num_channels, 1, 1, 1), groups=num_channels)
+        final_x = blurred_x / (y_gaussian.sum() * x_gaussian.sum())  # normalize
+        if self.cut_channel is not None:
+            final_x[:, self.cut_channel, ...] *= 0
+        final_x[:,:2,...]=final_x[:,:2,...] * (1-self.center_mask).to(x.device)
+
+        return final_x
 
 class ChangeNormAndClip:
     """ Change the norm of the input.
@@ -175,6 +235,63 @@ class ClipNormInChannel:
             x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
             return x
 
+class ClipNormInChannelSurr:
+    """ Change the norm of the input.
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, channel, key, norm=None, x_min=None, x_max=None, mask_thres=0.3):
+        self.channel = channel
+        self.norm = norm
+        self.x_min = x_min
+        self.x_max = x_max
+
+        src_method_fn = key["src_method_fn"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+        unit_id = key["unit_id"]
+
+        inner_mei_path = (MEI & dict(method_fn=src_method_fn) & dict(ensemble_hash=inner_ensemble_hash) & dict(method_hash=inner_method_hash) & dict(unit_id=unit_id)).fetch1('mei', download_path=fetch_download_path)
+        inner_mei=torch.load(inner_mei_path)
+        self.center_mask=(inner_mei[0][-1] > mask_thres) * 1
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x_norm = torch.norm(x[:, self.channel, ...] * (1-self.center_mask).to(x.device) )
+        if self.norm is not None:
+            if x_norm > self.norm:
+                x[:, self.channel, ...] = x[:, self.channel, ...] * (1-self.center_mask).to(x.device) * (self.norm / x_norm) + x[:, self.channel, ...] * self.center_mask.to(x.device)
+        if self.x_min is not None:
+            x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
+        return x
+
+class ClipNormInEveryChannel:
+    """ Change the norm of the input for every specified channel when generate transparent color MEI.
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, channel, norm, x_min=None, x_max=None):
+        self.channel = channel
+        self.norm = norm
+        self.x_min = x_min
+        self.x_max = x_max
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        for ch in range(len(self.channel)):
+            ch_norm = torch.norm(x[:, self.channel[ch], ...])
+            if self.norm[ch] != None:
+                if ch_norm > self.norm[ch]: # when actual norm larger than desired norm
+                    x[:, self.channel[ch], ...] = x[:, self.channel[ch], ...] * (self.norm[ch] / ch_norm)
+            if self.x_min[ch] != None:
+                x[:, self.channel[ch], ...] = torch.clamp(x[:, self.channel[ch], ...], self.x_min[ch], self.x_max[ch])
+        return x
 
 class ClipNormInChannelforRingFlexNorm:
     """ Change the norm of the input.
@@ -222,7 +339,6 @@ class ClipNormInChannelforRingFlexNorm:
         else:
            x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
         return x
-
 
 class ClipNormInChannelforRing:
     """ Change the norm of the input.
@@ -292,14 +408,11 @@ class ClipNormInChannelforSurround:
         unit_id = key["unit_id"]
 
         inner_mei_path = (MEI & dict(method_fn=src_method_fn) & dict(ensemble_hash=inner_ensemble_hash) & dict(method_hash=inner_method_hash) & dict(unit_id=unit_id)).fetch1('mei', download_path=fetch_download_path)
-        
         inner_mei=torch.load(inner_mei_path)
-
         self.center_mask=(inner_mei[0][1] > mask_thres) * 1
 
     @varargin
     def __call__(self, x, iteration=None):
-
         x_norm_ch1 = torch.norm(x[:, self.channel1, ...] * (1-self.center_mask).to(x.device)) 
         if self.norm_ch1 is not None:
             if x_norm_ch1 > self.norm_ch1:
@@ -431,7 +544,6 @@ class ChangeNormShuffleBehavior:
         x[:, self.first_behav_channel+1, ...] = np.random.uniform(self.dpupil_limits[0], self.dpupil_limits[1])
         x[:, self.first_behav_channel+2, ...] = np.random.uniform(self.treadmill_limits[0], self.treadmill_limits[1])
         return x
-
 
 class ChangeStdClampedMean:
     """ Change the norm of the input.
