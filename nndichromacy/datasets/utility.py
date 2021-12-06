5afb3add73792240474a8d714bd088816749986d
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-12-06 14:52:17 -0600

    remove print statements in PreprocessedMouse helper function

--- nndichromacy/datasets/utility.py
+++ nndichromacy/datasets/utility.py
@@ -357,9 +357,7 @@ def add_h5_to_preprocessed_table(
     else:
         datasets = filenames
 
-    print(datasets)
     for datafile in datasets:
-        print(datafile)
         if datafile.endswith(".h5"):
             with h5py.File(datafile) as fid:
                 print(datafile, fid["images"].shape)
@@ -375,19 +373,15 @@ def add_h5_to_preprocessed_table(
             filename = (template + "/").format(**key)
         else:
             filename = datasets[0].split(".")[0] + "/"
-        print(filename)
         dat = FileTreeDataset(filename, "images", "responses")
         dat.add_link("responses", "targets")
         dat.add_link("images", "inputs")
-        print(dat)
 
     for i, key in enumerate((experiment.Scan() & keys).fetch("KEY")):
-        print(key)
         if filenames is None:
             filename = (template + "/").format(**key)
         else:
             filename = datasets[i]
-        print(filename)
         dat = FileTreeDataset(filename, "images", "responses")
         ai, se, si, ui, x, y, z = (experiment.ScanSet.UnitInfo & key).fetch(
             "animal_id", "session", "scan_idx", "unit_id", "um_x", "um_y", "um_z"
@@ -400,7 +394,6 @@ def add_h5_to_preprocessed_table(
             filename = (template + "/").format(**key)
         else:
             filename = datasets[0].split(".")[0] + "/"
-        print(filename)
         dat = FileTreeDataset(filename, "images", "responses")
         dat.zip()
 
