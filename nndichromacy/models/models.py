fedf9a5ef977f0aa5828d43ce0454b28604f524c
KonstantinWilleke <konstantin.willeke@gmail.com>
2021-12-10 18:30:31 -0600

    Add CenterSurroundReadout.

--- nndichromacy/models/models.py
+++ nndichromacy/models/models.py
@@ -20,6 +20,7 @@ from .readouts import (
     MultipleSpatialXFeatureLinear,
     MultipleGaussian2d,
     MultipleFullGaussian2dBehav,
+    MultipleCenterSurround,
 )
 
 try:
@@ -1372,3 +1373,168 @@ def augmented_full_readout(
             model.readout[sessions[0]] = model.readout.pop("augmentation")
 
     return models
+
+
+def se_core_center_surround(
+        dataloaders,
+        seed,
+        hidden_channels=32,
+        input_kern=13,  # core args
+        hidden_kern=3,
+        layers=3,
+        gamma_input=15.5,
+        skip=0,
+        final_nonlinearity=True,
+        momentum=0.9,
+        pad_input=False,
+        batch_norm=True,
+        hidden_dilation=1,
+        laplace_padding=None,
+        input_regularizer="LaplaceL2norm",
+        init_mu_range=0.2,
+        readout_bias=True,  # readout args,
+        gamma_readout=4,
+        elu_offset=0,
+        stack=None,
+        se_reduction=32,
+        n_se_blocks=0,
+        depth_separable=False,
+        linear=False,
+        data_info=None,
+        attention_conv=False,
+        shifter=None,
+        shifter_type="MLP",
+        input_channels_shifter=2,
+        hidden_channels_shifter=5,
+        shift_layers=3,
+        gamma_shifter=0,
+        shifter_bias=True,
+        hidden_padding=None,
+        init_center_var=.02,
+        init_surround_var=.02,
+        init_surround_radius=.45,
+        center_on=True,
+        surround_on=True,
+
+):
+    """
+    Model class of a stacked2dCore (from neuralpredictors) and a pointpooled (spatial transformer) readout
+
+    Args:
+        dataloaders: a dictionary of dataloaders, one loader per session
+            in the format {'data_key': dataloader object, .. }
+        seed: random seed
+        elu_offset: Offset for the output non-linearity [F.elu(x + self.offset)]
+        isotropic: whether the Gaussian readout should use isotropic Gaussians or not
+        grid_mean_predictor: if not None, needs to be a dictionary of the form
+            {
+            'type': 'cortex',
+            'input_dimensions': 2,
+            'hidden_layers':0,
+            'hidden_features':20,
+            'final_tanh': False,
+            }
+            In that case the datasets need to have the property `neurons.cell_motor_coordinates`
+        share_features: whether to share features between readouts. This requires that the datasets
+            have the properties `neurons.multi_match_id` which are used for matching. Every dataset
+            has to have all these ids and cannot have any more.
+        all other args: See Documentation of Stacked2dCore in neuralpredictors.layers.cores and
+            PointPooled2D in neuralpredictors.layers.readouts
+
+    Returns: An initialized model which consists of model.core and model.readout
+    """
+
+    if data_info is not None:
+        n_neurons_dict, in_shapes_dict, input_channels = unpack_data_info(data_info)
+    else:
+        if "train" in dataloaders.keys():
+            dataloaders = dataloaders["train"]
+
+        # Obtain the named tuple fields from the first entry of the first dataloader in the dictionary
+        in_name, out_name = next(iter(list(dataloaders.values())[0]))._fields[:2]
+
+        session_shape_dict = get_dims_for_loader_dict(dataloaders)
+        n_neurons_dict = {k: v[out_name][1] for k, v in session_shape_dict.items()}
+        in_shapes_dict = {k: v[in_name] for k, v in session_shape_dict.items()}
+        input_channels = [v[in_name][1] for v in session_shape_dict.values()]
+
+    core_input_channels = (
+        list(input_channels.values())[0]
+        if isinstance(input_channels, dict)
+        else input_channels[0]
+    )
+
+    if "train" in dataloaders.keys():
+        dataloaders = dataloaders["train"]
+
+    if shifter is True:
+        data_keys = [i for i in dataloaders.keys()]
+        if shifter_type == "MLP":
+            shifter = MLPShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                hidden_channels_shifter=hidden_channels_shifter,
+                shift_layers=shift_layers,
+                gamma_shifter=gamma_shifter,
+            )
+
+        elif shifter_type == "StaticAffine":
+            shifter = StaticAffine2dShifter(
+                data_keys=data_keys,
+                input_channels=input_channels_shifter,
+                bias=shifter_bias,
+                gamma_shifter=gamma_shifter,
+            )
+
+    set_random_seed(seed)
+
+    core = SE2dCore(
+        input_channels=core_input_channels,
+        hidden_channels=hidden_channels,
+        input_kern=input_kern,
+        hidden_kern=hidden_kern,
+        layers=layers,
+        gamma_input=gamma_input,
+        skip=skip,
+        final_nonlinearity=final_nonlinearity,
+        bias=False,
+        momentum=momentum,
+        pad_input=pad_input,
+        batch_norm=batch_norm,
+        hidden_dilation=hidden_dilation,
+        laplace_padding=laplace_padding,
+        input_regularizer=input_regularizer,
+        stack=stack,
+        se_reduction=se_reduction,
+        n_se_blocks=n_se_blocks,
+        depth_separable=depth_separable,
+        linear=linear,
+        attention_conv=attention_conv,
+        hidden_padding=hidden_padding,
+    )
+
+    readout = MultipleCenterSurround(
+        core,
+        in_shape_dict=in_shapes_dict,
+        n_neurons_dict=n_neurons_dict,
+        init_mu_range=init_mu_range,
+        bias=readout_bias,
+        gamma_readout=gamma_readout,
+        init_center_var=init_center_var,
+        init_surround_var=init_surround_var,
+        init_surround_radius=init_surround_radius,
+        center_on=center_on,
+        surround_on=surround_on,
+    )
+
+    # initializing readout bias to mean response
+    if readout_bias and data_info is None:
+        for key, value in dataloaders.items():
+            _, targets = next(iter(value))[:2]
+            readout[key].bias.data = targets.mean(0)
+
+    model = GeneralEncoder(
+        core=core, readout=readout, elu_offset=elu_offset, shifter=shifter
+    )
+
+    return model
--- nndichromacy/models/utility.py
+++ nndichromacy/models/utility.py
@@ -7,3 +7,12 @@ def unpack_data_info(data_info):
     input_channels = [v["input_channels"] for k, v in data_info.items()]
     n_neurons_dict = {k: v["output_dimension"] for k, v in data_info.items()}
     return n_neurons_dict, in_shapes_dict, input_channels
+
+
+def cart2pol_torch(x, y):
+    """
+    Change cartesian coordinates to polar
+    """
+    rho = torch.sqrt(x**2 + y**2)
+    phi = torch.atan2(y, x)
+    return rho, phi
