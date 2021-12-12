5affbf96fe9f79d9bd83a504f558b8bb74155dc4
ShanqMa <shanqianma@gmail.com>
2021-12-12 14:51:53 +0100

    color surround MEI, ops function modification

--- nndichromacy/mei/ops_surroundMEI.py
+++ nndichromacy/mei/ops_surroundMEI.py
@@ -25,7 +25,7 @@ class RandomNormBehaviorPositionsSurr(InitialGuessCreator):
 
     _create_random_tensor = randn
 
-    def __init__(self, selected_channels, selected_values, key, mask_thres=0.3):
+    def __init__(self, selected_channels, selected_values, key):
         if not isinstance(selected_channels, Iterable) and (selected_channels is not None):
             selected_channels = (selected_channels)
 
@@ -67,6 +67,49 @@ class RandomNormBehaviorPositionsSurr(InitialGuessCreator):
     def __repr__(self):
         return f"{self.__class__.__qualname__}()"
 
+class RandomInitialBehaviorSurr(InitialGuessCreator):
+    """Used to create an initial guess tensor filled with values distributed according to a normal distribution."""
+
+    _create_random_tensor = randn
+
+    def __init__(self, selected_channels, key):
+        if not isinstance(selected_channels, Iterable) and (selected_channels is not None):
+            selected_channels = (selected_channels)
+
+        self.selected_channels = selected_channels
+        
+        src_method_fn = key["src_method_fn"]
+        unit_id = key["unit_id"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+        inner_mask_hash = key['mask_hash']
+
+        mei_key = dj.AndList([dict(method_fn=src_method_fn),
+                     dict(ensemble_hash=inner_ensemble_hash),
+                     dict(method_hash=inner_method_hash),
+                     dict(unit_id=unit_id)])
+
+        inner_mei_path = (MEI & mei_key).fetch1('mei', download_path=fetch_download_path)
+        inner_mei=torch.load(inner_mei_path)
+        
+        center_mask= (MEIThresholdMask & mei_key & dict(mask_hash=inner_mask_hash)).fetch1( "mask", download_path=fetch_download_path)
+        self.center_mask=torch.tensor(center_mask)
+
+        self.centerimg = inner_mei[0][:2]*self.center_mask
+        self.originalMEI = inner_mei
+
+    def __call__(self, *shape):
+        """Creates a random initial guess from which to start the MEI optimization process given a shape.
+        maintain the behavior channel same as source center MEI"""
+        initial = self._create_random_tensor(*shape)
+        behav_channel = self.originalMEI
+        # only change color channel
+        behav_channel[:,self.selected_channels,...]=self.centerimg + initial[:,self.selected_channels,...] * (1-self.center_mask)
+        return behav_channel
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}()"
+
 class BlurAndCutSurr:
     """ Blur an image with a Gaussian window.
 
@@ -80,7 +123,7 @@ class BlurAndCutSurr:
             'constant', 'reflect' and 'replicate'
     """
 
-    def __init__(self, sigma, key, decay_factor=None, truncate=4, pad_mode="reflect", cut_channel=None,mask_thres=0.3):
+    def __init__(self, sigma, key, decay_factor=None, truncate=4, pad_mode="reflect", cut_channel=None):
         self.sigma = sigma if isinstance(sigma, tuple) else (sigma,) * 2
         self.decay_factor = decay_factor
         self.truncate = truncate
@@ -136,14 +179,14 @@ class BlurAndCutSurr:
         return final_x
 
 class ClipNormInChannelSurr:
-    """ Change the norm of the input.
+    """ Change the norm of the input for surround part only.
 
     Arguments:
         norm (float or tensor): Desired norm. If tensor, it should be the same length as
             x.
     """
 
-    def __init__(self, channel, key, norm=None, x_min=None, x_max=None, mask_thres=0.3):
+    def __init__(self, channel, key, norm=None, x_min=None, x_max=None):
         self.channel = channel
         self.norm = norm
         self.x_min = x_min
@@ -154,8 +197,6 @@ class ClipNormInChannelSurr:
         inner_method_hash = key["inner_method_hash"]
         unit_id = key["unit_id"]
         inner_mask_hash = key['mask_hash']
-        #inner_mei_path = (MEI & dict(method_fn=src_method_fn) & dict(ensemble_hash=inner_ensemble_hash) & dict(method_hash=inner_method_hash) & dict(unit_id=unit_id)).fetch1('mei', download_path=fetch_download_path)
-        #inner_mei = torch.load(inner_mei_path)
 
         mask_key = dj.AndList([dict(method_fn=src_method_fn),
                      dict(ensemble_hash=inner_ensemble_hash),
@@ -174,3 +215,168 @@ class ClipNormInChannelSurr:
         if self.x_min is not None:
             x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
         return x
+
+
+class ClipNormInChannelSurr_totalctr:
+    """ Change the norm of the input for the total MEI norm.
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, channel, key, norm=None, x_min=None, x_max=None):
+        self.channel = channel
+        self.norm = norm
+        self.x_min = x_min
+        self.x_max = x_max
+
+        src_method_fn = key["src_method_fn"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+        unit_id = key["unit_id"]
+        inner_mask_hash = key['mask_hash']
+
+        mei_key = dj.AndList([dict(method_fn=src_method_fn),
+                     dict(ensemble_hash=inner_ensemble_hash),
+                     dict(method_hash=inner_method_hash),
+                     dict(unit_id=unit_id)])
+
+        inner_mei_path = (MEI & mei_key).fetch1('mei', download_path=fetch_download_path)
+        inner_mei=torch.load(inner_mei_path)
+        
+        center_mask= (MEIThresholdMask & mei_key & dict(mask_hash=inner_mask_hash)).fetch1( "mask", download_path=fetch_download_path)
+        self.center_mask=torch.tensor(center_mask)
+
+        self.centerimg = inner_mei[0][:2]*self.center_mask
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x_norm = torch.norm(x[:, self.channel, ...] * (1-self.center_mask).to(x.device) )
+        x[:, self.channel, ...] = x[:, self.channel, ...] * (1-self.center_mask).to(x.device) * (self.norm / x_norm) + x[:, self.channel, ...] * self.center_mask.to(x.device)
+        
+        # norm for (center + blurred surround):
+        c_norm = torch.norm(x[:, self.channel, ...] * (self.center_mask > 0).to(x.device))
+        # norm for nonblurred surround:
+        s_norm = torch.norm(x[:, self.channel, ...] * (self.center_mask == 0).to(x.device))
+        print(c_norm,' ,,, ',s_norm)
+        if self.norm is not None:
+            ######!!!!!!!
+            norm_surr=torch.sqrt(self.norm**2 - c_norm**2)
+            # only rescale nonblurred surround region
+            x[:, self.channel, ...] = x[:, self.channel, ...] * (self.center_mask == 0).to(x.device) * (norm_surr / s_norm) + x[:, self.channel, ...] * (self.center_mask>0).to(x.device)
+        if self.x_min is not None:
+            x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
+        return x
+
+class ClipNormInChannelSurr_ctr:
+    """ Change the norm of the surround such that total norm is stable.
+        works better than "ClipNormInChannelSurr"
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, channel, key, norm=None, x_min=None, x_max=None):
+        self.channel = channel
+        self.norm = norm
+        self.x_min = x_min
+        self.x_max = x_max
+
+        src_method_fn = key["src_method_fn"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+        unit_id = key["unit_id"]
+        inner_mask_hash = key['mask_hash']
+
+
+        mei_key = dj.AndList([dict(method_fn=src_method_fn),
+                     dict(ensemble_hash=inner_ensemble_hash),
+                     dict(method_hash=inner_method_hash),
+                     dict(unit_id=unit_id)])
+
+        inner_mei_path = (MEI & mei_key).fetch1('mei', download_path=fetch_download_path)
+        inner_mei=torch.load(inner_mei_path)
+        
+        center_mask= (MEIThresholdMask & mei_key & dict(mask_hash=inner_mask_hash)).fetch1( "mask", download_path=fetch_download_path)
+        self.center_mask=torch.tensor(center_mask)
+
+        self.centerimg = inner_mei[0][:2]*self.center_mask
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x_norm = torch.norm(x[:, self.channel, ...] * (1-self.center_mask).to(x.device) )        
+        x[:, self.channel, ...] = x[:, self.channel, ...] * (1-self.center_mask).to(x.device) * (self.norm / x_norm) + self.centerimg.to(x.device)
+        if self.x_min is not None:
+            x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
+        return x
+
+################################ TRANSFORMS ##############################################
+class RenormSurround:
+    """ Change the norm of the surround such that total norm is stable.
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+
+    def __init__(self, channel, key, norm=None):
+        self.channel = channel
+        self.norm = norm
+
+        src_method_fn = key["src_method_fn"]
+        inner_ensemble_hash = key["inner_ensemble_hash"]
+        inner_method_hash = key["inner_method_hash"]
+        unit_id = key["unit_id"]
+        inner_mask_hash = key['mask_hash']
+
+
+        mei_key = dj.AndList([dict(method_fn=src_method_fn),
+                     dict(ensemble_hash=inner_ensemble_hash),
+                     dict(method_hash=inner_method_hash),
+                     dict(unit_id=unit_id)])
+
+        inner_mei_path = (MEI & mei_key).fetch1('mei', download_path=fetch_download_path)
+        inner_mei=torch.load(inner_mei_path)
+        
+        center_mask= (MEIThresholdMask & mei_key & dict(mask_hash=inner_mask_hash)).fetch1( "mask", download_path=fetch_download_path)
+        self.center_mask=torch.tensor(center_mask)
+
+        self.centerimg = inner_mei[0][:2]*self.center_mask
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x_norm = torch.norm(x[:, self.channel, ...] * (1-self.center_mask).to(x.device) )  
+        
+        #print('MEI gradien',x.grad)
+        renorm_x = torch.clone(x).requires_grad_(True)
+        renorm_x[:, self.channel, ...] = x[:, self.channel, ...] * (1-self.center_mask).to(x.device) * (self.norm / x_norm) + self.centerimg.to(x.device)
+        #print('MEI gradient',x.grad)
+        #print('rescaled MEI gradient',renorm_x)
+        ### here x should be related to renorm_x.grad???
+        # y=(2*x+2)
+        #print("y is leaf:,", y.is_leaf)
+        print("transformed x is leaf: ",renorm_x.is_leaf)
+        return renorm_x
+
+################################ POSTPROCESSING ##############################################
+class ClipSurround:
+    """ Change the norm of the surround such that total norm is stable.
+        works better than "ClipNormInChannelSurr"
+
+    Arguments:
+        norm (float or tensor): Desired norm. If tensor, it should be the same length as
+            x.
+    """
+    def __init__(self, channel, x_min=None, x_max=None):
+        self.channel = channel
+        self.x_min = x_min
+        self.x_max = x_max
+
+    @varargin
+    def __call__(self, x, iteration=None):
+        x[:, self.channel, ...] = torch.clamp(x[:, self.channel, ...], self.x_min, self.x_max)
+        return x
+
+
